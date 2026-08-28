b15c13d18d0abd7d0c365495638b49f7ec18b9cb
EmilianoAllende <emilianoallende94@gmail.com>
2026-08-28 02:58:10 -0300

    Forzar IPv4 para arreglar DNS en Render

--- ProyMod2/back/src/config/dbConfig.js
+++ ProyMod2/back/src/config/dbConfig.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
+﻿const mongoose = require("mongoose");
 
 const dbCon = async () => {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, { family: 4 });
 };
 
 module.exports = dbCon;
