31344ff8ac29430f002edc6bff8dafdecceb4f07
EmilianoAllende <emilianoallende94@gmail.com>
2026-08-28 03:14:29 -0300

    myURL fixed.

--- ProyMod2/front/utils/constants.js
+++ ProyMod2/front/utils/constants.js
@@ -1,5 +1,5 @@
 const URL = "https://students-api.2.us-1.f10.io/movies";
 const URL2 = "https://students-api.up.railway.app/movies";
-const myURL = "http://localhost:3000/movies";
+const myURL = "https://proymod2.onrender.com/movies";
 
 module.exports = { URL, URL2, myURL };
