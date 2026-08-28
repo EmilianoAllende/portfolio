0113bf4296d98dab92c39cd2795b9c16355511d9
EmilianoAllende <emilianoallende94@gmail.com>
2026-08-28 05:21:27 -0300

    Fix JWT expiresIn type error

--- ProyModFinal/back/src/app.module.ts
+++ ProyModFinal/back/src/app.module.ts
@@ -61,7 +61,7 @@ import { SharedModule } from './common/services/shared.module';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') as any },
       }),
       global: true,
     }),
