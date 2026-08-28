12b6b9282040a8f108fb3930759ba0a2815d1b54
EmilianoAllende <emilianoallende94@gmail.com>
2026-08-28 05:39:32 -0300

    Fix Google Client Strategy missing clientID crash

--- ProyModFinal/back/src/modules/auth/strategies/google-client.strategy.ts
+++ ProyModFinal/back/src/modules/auth/strategies/google-client.strategy.ts
@@ -10,8 +10,8 @@ export class GoogleClientStrategy extends PassportStrategy(
 ) {
   constructor(private readonly configService: ConfigService) {
     super({
-      clientID: configService.get<string>('GOOGLE_CLIENT_ID')!,
-      clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET')!,
+      clientID: configService.get<string>('GOOGLE_CLIENT_ID') || 'dummy_client_id',
+      clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET') || 'dummy_client_secret',
       callbackURL: configService.get<string>('GOOGLE_CALLBACK_URL_CLIENT'),
       scope: ['email', 'profile'],
     });
